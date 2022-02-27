6b8f72dcabbad13abfc4164d1dce30b79aeb8bc4
Sangtaek-Lee <ttaekki7@gmail.com>
2022-02-27 23:07:56 +0900

    0227

--- ect_/Problem/03_algorithm/Algorithm_IM/LED_Light/sol1.py
+++ ect_/Problem/03_algorithm/Algorithm_IM/LED_Light/sol1.py
@@ -6,14 +6,23 @@ T = int(input())
 
 def min_cnt(N, target):
     led = [0]*N
-    for i in range(2, N+1):           # 1의 배수는 해당사항 없으니 2의 배수 부터
-        # print(i)
-        while i < N + 1:
-            print(i)
-
-            i = i + i
-            # print(i)
-
+    cnt = 0
+    if target[0] == 1:
+        led[0] = 1
+        cnt += 1
+    for i in range(2, N+1):             # 0, 1의 배수는 해당사항 없으니 2의 배수 부터 (2, 3, 4, 5)
+        if led[i - 1] != target[i - 1]:            # index 는(1, 2, 3, 4)
+            print(f'i:{i}')
+            k = i
+            cnt += 1                    # cnt 증가
+            if led[i - 1] == 0:
+                while k < N + 1:
+                    led[k - 1] = 1
+                    k *= 2
+            else:
+                while k < N + 1:
+                    led[k - 1] = 0
+                    k *= 2
         # list와 비교하여 맞는지 비교하여 cnt 값 리턴
         if target == led:
             return cnt
