eac5856a8057296ba7db4c4e5eaf331bb394c2a7
Sangtaek-Lee <ttaekki7@gmail.com>
2022-05-09 23:45:57 +0900

    0509

--- ect_/Problem/Django/workshop_0421/articles/serializers.py
+++ ect_/Problem/Django/workshop_0421/articles/serializers.py
@@ -2,15 +2,20 @@ from rest_framework import serializers
 from .models import Artist, Music
 
 
-# class CommentListSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Comment
-#         fields = '__all__'
-#         read_only_fields = ('article',)
+class MusicListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Music
+        fields = '__all__'
+
+class MusicListSerializer(serializers.ModelSerializer):
+    artist = Music.objects.artist(many=True, read_only=True)
+    class Meta:
+        model = Music
+        fields = '__all__'
 
 # class ArticleDetailSerializer(serializers.ModelSerializer):
 #     # comment_set = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-#     comment_set = CommentListSerializer(many=True, read_only=True)
+#     comment_set = MusicListSerializer(many=True, read_only=True)
 #     class Meta:
 #         model = Article
 #         fields = '__all__'
@@ -23,7 +28,7 @@ class ArtistListSerializer(serializers.ModelSerializer):
         fields = ('id', 'name',)
 
 class ArtistSerializer(serializers.ModelSerializer):
-    music_set = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    music_set = MusicListSerializer(many=True, read_only=True)
     music_count = music_set.count()
 
     class Meta:
